f5d922f2dcc30e74d7ee745c460067f0f0dd3366
ashley hwacha <ashleyhwacha@gmail.com>
2022-06-26 22:12:32 +0200

    added ejs templating

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // register view engine
 app.set('view engine', 'ejs');
-
+ 
 const PORT = 3000;
 
 //listen for requests
@@ -16,20 +16,19 @@ app.listen(
 
 app.get('/',(req, res)=>{
 //  res.send('<p>home page</p>');
- res.sendFile('./views/index.html', {root: __dirname});
+ res.render('index', { title: 'Home'});
 } );
 
 app.get('/about',(req, res)=>{
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+   res.render('about');
    } );
 
-// redirects
-app.get('/about-us', (re, res)=>{
-    res.redirect('/about');
-} );
+app.get('/blogs/create', (req, res)=>{
+  res.render('create'); 
+});
 
 //404 page
 app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 });
